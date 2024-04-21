ab14041529118c8dcdd2c1f6815dafd1851a7667
Anna Semsey <asemsey@2-B-6.42heilbronn.de>
2024-04-21 13:34:48 +0200

    heredoc

--- parse/get_fd.c
+++ parse/get_fd.c
@@ -6,11 +6,28 @@
 /*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:31:27 by asemsey           #+#    #+#             */
-/*   Updated: 2024/04/20 12:11:30 by asemsey          ###   ########.fr       */
+/*   Updated: 2024/04/21 13:32:42 by asemsey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	write_heredoc(char *del, int fd)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("> ");
+		if (!ft_strcmp(line, del))
+		{
+			free(line);
+			break ;
+		}
+		ft_putendl_fd(line, fd);
+		free(line);
+	}
+}
 
 void	get_fd(char *str, t_type type, int *fd_in, int *fd_out)
 {
@@ -25,13 +42,18 @@ void	get_fd(char *str, t_type type, int *fd_in, int *fd_out)
 	if ((type == OUT || type == APPEND) && *fd_out != STDOUT_FILENO)
 		close(*fd_out);
 	if (type == OUT)
-		*fd_out = open(str, O_RDWR | O_TRUNC | O_CREAT);
+		*fd_out = open(str, O_RDWR | O_TRUNC | O_CREAT, 0644);
 	if (type == APPEND)
-		*fd_out = open(str, O_RDWR | O_APPEND | O_CREAT);
+		*fd_out = open(str, O_RDWR | O_APPEND | O_CREAT, 0644);
 	if ((type == IN || type == HEREDOC) && *fd_in != STDIN_FILENO)
 		close(*fd_in);
 	if (type == IN)
 		*fd_in = open(str, O_RDONLY);
+	if (type == HEREDOC)
+	{
+		*fd_in = open(".heredoc", O_RDWR | O_TRUNC | O_CREAT, 0644);
+		write_heredoc(str, *fd_in);
+	}
 }
 
 void	set_cmd_fd(t_cmd *cmd)
@@ -43,7 +65,7 @@ void	set_cmd_fd(t_cmd *cmd)
 	i = 0;
 	while (arg)
 	{
-		if (cmd->type[i] != ARG && cmd->type[i] != HEREDOC)
+		if (cmd->type[i] != ARG)
 		{
 			get_fd((char *)arg->content, cmd->type[i], \
 				&cmd->fd_in, &cmd->fd_out);
